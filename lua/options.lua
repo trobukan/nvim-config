7fe8c0f1b52a20c0491630b789eaee57d93af5d0
trobukan <trobukan@gmail.com>
2025-07-26 20:34:29 -0300

    update: options and mappings

--- lua/options.lua
+++ lua/options.lua
@@ -1,4 +1,24 @@
 require "nvchad.options"
 
 local o = vim.o
-o.cursorlineopt = "both" -- to enable cursorline!
+
+o.laststatus = 3
+o.number = true
+o.showmode = true
+o.sliptkeep = "screen"
+o.cursorline = true
+o.cursorlineopt = "number"
+o.expandtab = true
+o.smartindent = true
+
+o.ignorecase = true
+o.smartcase = true
+o.incsearch = true
+o.hlsearch = true
+
+o.relativenumber = true
+o.numberwidth = 3
+
+o.termguicolors = true
+o.wrap = false
+o.scrolloff = 8
