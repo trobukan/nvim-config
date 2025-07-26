50ece3ba5bf9985000ee01ec286e82c9a952f14b
trobukan <trobukan@gmail.com>
2025-07-26 16:08:39 -0300

    add plugins and add format_save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -5,11 +5,10 @@ local options = {
     -- html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+  timeout_ms = 500,
+  lsp_fallback = true,
+ },
 }
 
 return options
